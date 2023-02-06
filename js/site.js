8d2875787b07a240b78f0d3b00bf81b84e2d2923
Zsolt Dzsugán <zsoltd.business@gmail.com>
2023-02-06 12:50:02 +0100

    initial commit
    some links not set up properly
    code page incomplete

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-// Call function
-function HelloWorld() {
-    alert("Hello World");
+// Get elements from page
+const btnRun = document.querySelector('#btnSubmit');
+const result = document.querySelector('#result');
+const alertMsg = document.querySelector('#alert');
+
+// Get String from the page
+// Controller function
+function getValue() {
+    alertMsg.classList.add("invisible");
+
+    let inputString = document.querySelector('#inputString').value;
+    let revString = reverseString(inputString);
+
+    displayString(revString);
 }
+// Reverse the string
+// Logic function
+function reverseString(inputString) {
+    let revString = [];
+
+    // Reverse a string using a for loop
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        revString += inputString[i];
+    }
+
+    return revString;
+}
+// Display reversed string to user
+// Display function
+function displayString(revString) {
+    result.innerHTML = `${revString}`;
+    alertMsg.classList.remove("invisible");
+}
+
+btnRun.addEventListener('click', getValue);
